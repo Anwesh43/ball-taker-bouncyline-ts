2a0485f4e616acefe193a76da8d1dcf96149b794
Anwesh43 <anweshthecool0@gmail.com>
2019-12-05 15:15:40 +0530

    adding BallBouncyTakeLine class

--- index.ts
+++ index.ts
@@ -188,3 +188,27 @@ class BBLNode {
         return this
     }
 }
+
+class BallTakerBouncyLine {
+
+    root : BBLNode = new BBLNode(0)
+    curr : BBLNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
